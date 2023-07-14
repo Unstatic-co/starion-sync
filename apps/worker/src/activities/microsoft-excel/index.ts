4ec54fcd8fe6bf05857573b6c3aa363fefd91f12
Ha Hoai <hahoai@unstatic.co>
2023-07-14 13:35:46 +0700

    chore: fix export

--- apps/worker/src/activities/microsoft-excel/index.ts
+++ apps/worker/src/activities/microsoft-excel/index.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class MicrosoftExcelActivities {
-  constructor() {}
+  constructor(private readonly configService: ConfigService) {}
 
   async greeting(name: string): Promise<string> {
     return `Hello, ${name}`;
