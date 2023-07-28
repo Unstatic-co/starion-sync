d42d87d65aa29dc019b21616c4714058897bb6a2
Ha Hoai <hahoai@unstatic.co>
2023-07-28 09:24:42 +0700

    feat(cron-trigger): run immediately

--- apps/triggers/cron/src/modules/trigger/trigger.processors.ts
+++ apps/triggers/cron/src/modules/trigger/trigger.processors.ts
@@ -2,9 +2,9 @@ import { Processor, Process } from '@nestjs/bull';
 import { Job } from 'bull';
 import { QUEUES } from '../../common/queues';
 import { Inject, Injectable, Logger } from '@nestjs/common';
-import { BrokerService } from '../broker/broker.service';
-import { EventNames, SyncConnection } from '@lib/core';
+import { SyncConnection } from '@lib/core';
 import { ITriggerRepository, InjectTokens } from '@lib/modules';
+import { TriggerService } from './trigger.service';
 
 @Processor(QUEUES.CRON_TRIGGER)
 @Injectable()
@@ -12,9 +12,9 @@ export class CronTriggerProcessor {
   private readonly logger = new Logger(CronTriggerProcessor.name);
 
   constructor(
-    private readonly brokerService: BrokerService,
     @Inject(InjectTokens.TRIGGER_REPOSITORY)
     private readonly triggerRepository: ITriggerRepository,
+    private readonly triggerService: TriggerService,
   ) {}
 
   @Process()
@@ -23,12 +23,9 @@ export class CronTriggerProcessor {
     const triggerId = (job.data as SyncConnection).id;
     const trigger = await this.triggerRepository.getById(triggerId);
     if (trigger) {
-      this.logger.debug(`trigger workflow ${trigger.workflow.id}`);
-      await this.brokerService.emitEvent(EventNames.WORKFLOW_TRIGGERED, {
-        payload: job.data,
-      });
+      await this.triggerService.processTrigger(trigger);
     } else {
-      this.logger.debug(`trigger ${triggerId} not found`);
+      this.logger.log(`trigger ${triggerId} not found`);
     }
   }
 }
