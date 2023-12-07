26127d6d9438aa4cfdf2a512c027812029e79ef0
Ha Hoai <hahoai@unstatic.co>
2023-12-07 14:39:07 +0700

    infra: add redis

--- infra/modules/flyio/main.tf
+++ infra/modules/flyio/main.tf
@@ -1,6 +1,6 @@
 locals {
   test_app_count        = var.is_production ? 0 : 0
-  redis_count           = var.is_production ? 0 : 0 ##
+  redis_count           = var.is_production ? 0 : 1 ##
   mongodb_count         = var.is_production ? 0 : 1 ##
   postgres_count        = var.is_production ? 0 : 0
   apps_count            = var.is_production ? 0 : 1 #
