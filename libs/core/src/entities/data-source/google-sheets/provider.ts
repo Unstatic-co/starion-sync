10c5e31c0c51ef7f9a5dc44c523a475514fd74e6
abcvjp <31028491+abcvjp@users.noreply.github.com>
2024-01-15 11:18:38 +0700

    chore(worker): no longer get spreadsheet data (#96)

--- libs/core/src/entities/data-source/google-sheets/provider.ts
+++ libs/core/src/entities/data-source/google-sheets/provider.ts
@@ -2,11 +2,4 @@ import { ProviderState } from '../dataProvider.entity';
 
 export interface GoogleSheetsProviderState extends ProviderState {
   downloadedAt: Date;
-  timeZone: string;
-  sheets: {
-    [sheetId: string]: {
-      name: string;
-      index: number; // from 0
-    };
-  };
 }
