0c242d8b144dea7ec213df1388b66f812d1ace34
abcvjp <31028491+abcvjp@users.noreply.github.com>
2024-01-29 14:41:38 +0700

    Fix/loader (#105)
    * infra(loader): incr metric
    
    * fix: google sheet name quoted
    
    * fix: special sheet names

--- apps/processors/downloader/util/sheet.go
+++ apps/processors/downloader/util/sheet.go
@@ -1,8 +1,12 @@
 package util
 
 import (
+	"bytes"
 	"fmt"
+	"log"
 	"strings"
+
+	excelize "github.com/xuri/excelize/v2"
 )
 
 // func NormalizeSheetName(sheetName string) string {
@@ -18,4 +22,24 @@ import (
 
 func FormatSheetNameInRange(sheetName string) string {
 	return fmt.Sprintf("'%s'", strings.ReplaceAll(sheetName, "'", "''"))
+}
+
+func GetSheetNamesFromXlsxData(fileBytes []byte) []string {
+	var sheetNames []string
+
+	// Open the Excel file
+	xlsx, err := excelize.OpenReader(bytes.NewReader(fileBytes))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get all sheet names
+	sheetList := xlsx.GetSheetList()
+
+	// Iterate through sheet names and print them
+	for _, sheetName := range sheetList {
+		sheetNames = append(sheetNames, sheetName)
+	}
+
+	return sheetNames
 }
