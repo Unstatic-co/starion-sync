4b3206e56f53a860e856dc90c6c6260589974c62
Ha Hoai <hahoai@unstatic.co>
2024-02-19 12:17:46 +0700

    fix(infra): ggcloud output

--- infra/modules/googlecloud/output.tf
+++ infra/modules/googlecloud/output.tf
@@ -1,15 +1,15 @@
-output "downloader_url" {
-  value = google_cloud_run_service.downloader.status[0].url
-}
+# output "downloader_url" {
+  # value = google_cloud_run_service.downloader.status[0].url
+# }
 
-output "comparer_url" {
-  value = google_cloud_run_service.comparer.status[0].url
-}
+# output "comparer_url" {
+  # value = google_cloud_run_service.comparer.status[0].url
+# }
 
-output "loader_url" {
-  value = google_cloud_run_service.loader.status[0].url
-}
+# output "loader_url" {
+  # value = google_cloud_run_service.loader.status[0].url
+# }
 
-output "metadata_url" {
-  value = google_cloud_run_service.metadata.status[0].url
-}
+# output "metadata_url" {
+  # value = google_cloud_run_service.metadata.status[0].url
+# }
