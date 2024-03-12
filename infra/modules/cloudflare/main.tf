f7163cb27b5352d98bf0c115733744472e396c14
Ha Hoai <hahoai@unstatic.co>
2024-03-12 15:18:20 +0700

    fix(infra): r2

---
+++ infra/modules/cloudflare/main.tf
@@ -0,0 +1,3 @@
+locals {
+  is_production            = var.environment == "production" ? true : false
+}
--- infra/modules/cloudflare/storage.tf
+++ infra/modules/cloudflare/storage.tf
@@ -1,5 +1,5 @@
 resource "cloudflare_r2_bucket" "sync_data" {
   account_id = var.cf_account_id
-  name       = "${var.project}-${var.environment}-sync-data"
+  name       =  local.is_production ? "${var.project}-${var.environment}-sync-data" : "${var.project}-${var.environment}-sync-data-new"
   # location   = "enam"
 }
